155348b2b57d62debc069e46397ce9b5039df06b
Juan Villatoro <smis010420@ugb.edu.sv>
2021-10-04 10:32:15 -0600

    Modificamos la View para que nos muestre el OrderID

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -24,3 +24,24 @@ ON p.CategoryID = c.CategoryID;
 
 SELECT
 Producto, Categoria, Proveedor FROM listadoProductos ORDER BY Categoria;
+
+--Modificar la vista anterior de tal manera que
+--muestre los productos que esten o no inlcuidos en las ordenes
+--muestre: nombre producto, nombre categoria, y el order id
+
+ALTER VIEW listadoProductos
+AS
+SELECT p.ProductName AS 'Producto', 
+c.CategoryName AS 'Categoria',
+o.OrderID AS 'Orden'
+FROM Products p
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID
+LEFT JOIN Orders o
+ON p.SupplierID = o.OrderID;
+
+
+SELECT
+Producto, Categoria, Orden FROM listadoProductos ORDER BY Categoria;
+
+--Modificar la vista anterior utilizando full join
