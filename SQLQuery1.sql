5151c2c00bac7735a4a79c6ee4d229f649fe54b2
Juan Villatoro <smis010420@ugb.edu.sv>
2021-10-04 10:06:32 -0600

    Se agregó una Vista llamada listadoProducto

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,26 @@
 USE Northwind;
+
+SELECT 
+	p.ProductName producto,
+	c.CategoryName categoria
+FROM
+	Products p
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID;
+
+--Crear una vista llamada listadoProductos que muestre
+--el nombre del producto, categoria y proveedor
+--ordenados por nombre de categoria
+
+CREATE VIEW listadoProductos
+AS
+SELECT p.ProductName AS 'Producto', 
+c.CategoryName AS 'Categoria',
+p.SupplierID AS 'Proveedor'
+FROM Products p
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID;
+
+
+SELECT
+Producto, Categoria, Proveedor FROM listadoProductos ORDER BY Categoria;
